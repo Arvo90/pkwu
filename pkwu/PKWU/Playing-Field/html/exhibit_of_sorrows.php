aafd82444e3daa0b03465fc09187438bc4070780
Arvo90 <156929298+Arvo90@users.noreply.github.com>
2024-05-10 21:28:20 +0700

    Add files via upload

---
+++ pkwu/PKWU/Playing-Field/html/exhibit_of_sorrows.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Playing Field</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/nav.css">
+    <link rel="stylesheet" href="../CSS/ingame2.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        integrity="sha384-lc5r+4Lq3V0gVhPQrVbY5te1N+27XWnJ6N3/xCBVl5vKU4N0CTxEgOiUn6rI5R6U" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <nav>
+        <ul>
+            <li>
+                <div class="logo-icon">
+                    <img src="../img/navIcon/logo.svg" class="logo" alt="">
+                    <span class="nav-item">Playing Field</span>
+                </div>
+            </li>
+            <div class="nav-line"></div>
+            </a>
+            <div class="isi-nav">
+                <li>
+                    <a href="#" class="active" onclick="changePage(this)">
+                        <img src="../img/navIcon/home.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="homet">Home</div>
+                        </span>
+                    </a>
+                </li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/game.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="gamet">Game</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/contact.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="contactt">Contact</div>
+                        </span>
+                    </a></li>
+            </div>
+            <div class="nav-line" style="margin-top: 5px;"></div>
+            <div class="isi-nav">
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/action.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="actiont">Action</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/shooter.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="aboutt">Shooter</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/racing.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="contactt">Racing</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/sport.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="actiont">Sport</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/horror.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="aboutt">Horror</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/puzzle.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="contactt">Puzzle</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/rpg.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="actiont">RPG</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/strategi.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="aboutt">Strategy</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/multiplayer.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="contactt">Multiplayer</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/arcade.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="actiont">Arcade</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/card.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="aboutt">Card</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/stickman.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="contactt">Stickman</div>
+                        </span>
+                    </a></li>
+                <li><a href="#" onclick="changePage(this)">
+                        <img src="../img/navIcon/casual.svg" class="nav-icon">
+                        <span class="nav-item">
+                            <div class="actiont">Casual</div>
+                        </span>
+                    </a></li>
+            </div>
+            <div class="nav-line" style="margin-top: 5px; margin-bottom: 7px;"></div>
+            <div class="logoSolid-icon" style="bottom: 0 auto; display:flex; ">
+                <span class="logo-span">playingfield.com</span>
+            </div>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="col-12">
+            <iframe src="https://www.crazygames.co.id/embed/exhibit-of-sorrows"
+                style="width: 100%; height:723px;border-radius: 20px; background-color: #083358" frameborder="0" allow="gamepad *;"></iframe>
+        </div>
+        <div class="row">
+            <div class="col-9 mt-3">
+                <div class="col-12 px-5 p-3" style=" background: #083358; border-radius: 20px">
+                    <div class="col-12 pb-3"
+                        style="color: #ffedfa; font-size: 2.5em; font-family: 'Circular Medium', sans-serif; letter-spacing: 2.94px; word-wrap: break-word">
+                        Sprout Valley</div>
+                    <div class="row">
+                        <div class="col-4"
+                            style="color: #98D0FF;font-size: 18px;letter-spacing: 2.1px; font-family: 'Circular Bold', sans-serif;">
+                            <div class="col-12 pb-3">
+                                Rating :
+                            </div>
+                            <div class="col-12 pb-3">
+                                Developer :
+                            </div>
+                            <div class="col-12 pb-3">
+                                Released :
+                            </div>
+                            <div class="col-12 pb-3">
+                                Engine :
+                            </div>
+                            <div class="col-12 pb-3">
+                                Platform :
+                            </div>
+                            <div class="col-12 pb-3">
+                                Tags :
+                            </div>
+                        </div>
+                        <div class="col-8"
+                            style="color: #FFEDFA;font-size: 18px;letter-spacing: 2.1px; font-family: 'Circular Bold', sans-serif;">
+                            <div class="col-12 pb-3">
+                                8.4
+                            </div>
+                            <div class="col-12 pb-3" style="color: #00F2F2;">
+                                Ibnu Studio
+                            </div>
+                            <div class="col-12 pb-3">
+                                12 July 2022
+                            </div>
+                            <div class="col-12 pb-3">
+                                HTML5(Construct 2)
+                            </div>
+                            <div class="col-12 pb-3">
+                                Browser(Desktop), Play Store(Android)
+                            </div>
+                            <div class="col-12 pb-3">
+                                njier
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="mb-4 mt-3" style=" width:100%; height: 2px;
+                    background-color: #3E7CB0;
+                    opacity: 70%;
+                    border: none;  margin:0 auto">
+                    <div class="col-12" style="color: #ffedfa;
+                font-size: 25px;
+                font-style: normal;
+                max-width: 750px;
+                font-weight: 500;
+                line-height: 149.991%;
+                padding-bottom: 100px;
+                font-family: 'Circular Medium', sans-serif;
+                letter-spacing: 1.8px;">Sprout Valley adalah game simulator pertanian yang menawan di mana 
+                                        Anda dapat melarikan diri dari keramaian dan kegaduhan kehidupan sehari-hari
+                                        serta tenggelam dalam ketenangan alam. Dalam game yang memikat ini, Anda berperan
+                                        sebagai Nico, seekor kucing lucu yang sedang mencari tempat yang damai 
+                                        untuk dipanggil sebagai rumah. Sepanjang perjalanan, Nico akan berteman, 
+                                        mengungkap rahasia, dan memulai perjalanan penemuan di dunia mempesona Ostara. 
+                                        Apakah Anda siap menanam benih surga Anda?</div>
+
+                </div>
+            </div>
+        </div>
+        <!---Alvaro-->
+        <div class="Other_Games col-3" style="overflow: hidden;">Other Games</div>
+        <span class="out_container">
+        <?php 
+        include "connection.php";
+
+        $sql= "SELECT * FROM other_games ; ";
+        $result= mysqli_query($connection, $sql);
+        if (mysqli_num_rows($result) >0) {
+            while ($row= mysqli_fetch_assoc($result)){
+           echo  "<div class='other_game_row'>"; 
+           ?>
+           <div class="other_game_card" onclick="changePageGame('<?php echo $row['url']; ?>')">
+        <div class="other_game_card-front">
+             <img src="../img/cardImg/<?php echo $row['image']; ?>" style=" height: 200px; width: 300px ;border-radius: 50px;">
+            
+        </div>
+        <div class="other_game_card-back">
+            <h2>Smash Kartz</h2>
+        </div>
+    </div> <?php
+            }
+        }
+        ?>
+        </span>
+
+
+
+
+    
+    
+
+   
+
+</div> <!--row-->
+
+
+    </div>
+    <footer class="w-100 py-4 flex-shrink-0" style="background: #083358;">
+        <div class="row">
+            <div class="col-5 mb-5 d-flex flex-column justify-content-between">
+                <div class="col-12" style="margin-top: 2em; margin-left: 4rem; ">
+                    <img src="../img/navIcon/logo.svg" style="width: 67px; height: 67px;" alt="">
+                </div>
+                <div class="col-12" style="margin-left: 4rem;">
+                    <div class="EmailUsAt"
+                        style=" color: #DADADA; font-size: 20px; font-family: Raleway; font-weight: 500; word-wrap: break-word">
+                        Email us at</div>
+                    <div class="PlayingfieldGmailCom"
+                        style="color: #DADADA; font-size: 20px; font-family: Raleway; font-weight: 500; word-wrap: break-word">
+                        playingfield@gmail.com</div>
+                </div>
+            </div>
+            <div class="col-7 mb-5">
+                <div class="col-12 float-start"
+                    style="margin-top: 0.7em; color: #DADADA; font-size: 35px;text-align: left; font-family: Raleway; font-weight: 700; letter-spacing: 3.50px; word-wrap: break-word  ">
+                    PLAYING FIELD
+                </div>
+                <div class="col-12 float-start"
+                    style="margin-top: 1.7em; padding-right: 2em; color: #DADADA; max-width: 850px; text-align: justify; font-size: 16px; font-family: 'Oxygen', sans-serif; font-weight: 500; letter-spacing: 1.60px; word-wrap: break-word ">
+                    ©2023 Playing Field. All Rights Reserved. All content on this site is protected by copyright law and
+                    owned by [Company Name or Site Owner's Name]. Unauthorized use, reproduction, or distribution is
+                    strictly prohibited.Trademarks and brand names belong to their respective owners. Use of this site
+                    does not grant rights to these trademarks.For inquiries or content usage permissions, contact us at
+                    [Email Address or Phone Number].
+                </div>
+                <div class="col-12 float-start" style=" margin-top: 3em; padding-right: 2em;">
+                    <div class="col-3" style="display: flex; justify-content: space-between; ">
+                        <img src="../img/footerIcon/ig.svg" style="width: 20px;" alt="">
+                        <img src="../img/footerIcon/ig.svg" style="width: 20px;" alt="">
+                        <img src="../img/footerIcon/ig.svg" style="width: 20px;" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr style=" width:94%; height: 2px;
+        background-color: #DADADA;
+        opacity: 100%;
+        border: none;  margin:0 auto">
+        <div class="row d-flex align-content-center" style="height: 7em;">
+            <div class="col-6">
+                <div class="col-6 float-start d-flex justify-content-between justify-content-start align-content-center"
+                    style=" margin-left: 4rem; text-align: center; color: #DADADA; font-size: 18px; font-family: 'Oxygen', sans-serif; font-weight: 500; line-height: 57.50px; letter-spacing: 3.84px;   ">
+                    <span>
+                        HOME
+                    </span>
+                    <span>
+                        ABOUT US
+                    </span>
+                    <span>
+                        CONTACT
+                    </span>
+                </div>
+            </div>
+            <div class="col-6 d-flex align-content-center">
+                <div class="col-12 float-end justify-content-end d-flex  align-content-center"
+                    style="padding-right: 4rem; text-align: center; color: #DADADA; font-size: 17px; font-family: 'Oxygen', sans-serif; font-weight: 700; letter-spacing: 3.57px; ">
+                    <span style="margin-top: auto;
+                    margin-bottom: auto;">Thank you for visiting Playing Field.</span>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script>
+
+        function changePageGame(fileName) {
+            window.location.href = fileName; // Navigate to the page with the specified file name
+        }
+
+        function changePage(element) {
+            // Remove 'active' class from all links
+            document.querySelectorAll('.isi-nav a').forEach(link => link.classList.remove('active'));
+
+            // Add 'active' class to the clicked link
+            element.classList.add('active');
+        }
+
+        const navElement = document.querySelector('nav');
+
+        navElement.addEventListener('mouseover', function () {
+            this.classList.add('scroll-hide-scrollbar');
+        });
+
+        navElement.addEventListener('mouseout', function () {
+            this.classList.remove('scroll-hide-scrollbar');
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
