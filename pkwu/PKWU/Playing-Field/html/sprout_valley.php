6207c6f249ffbc4e9a479de58c97302f65c35a92
Arvo90 <156929298+Arvo90@users.noreply.github.com>
2024-05-13 07:41:12 +0700

    Update sprout_valley.php

--- pkwu/PKWU/Playing-Field/html/sprout_valley.php
+++ pkwu/PKWU/Playing-Field/html/sprout_valley.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../CSS/nav.css">
-    <link rel="stylesheet" href="../CSS/ingame2.css">
+    <link rel="stylesheet" href="../CSS/ingame_true.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
         integrity="sha384-lc5r+4Lq3V0gVhPQrVbY5te1N+27XWnJ6N3/xCBVl5vKU4N0CTxEgOiUn6rI5R6U" crossorigin="anonymous">
